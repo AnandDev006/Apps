859591d69f86a9713840a59091761c41c737321c
AnandDev006 <anand.dev.006@gmail.com>
2019-04-27 08:48:47 +0530

    a

---
+++ notes/notes-backend/routes/users.js
@@ -0,0 +1,34 @@
+const router = require('express-promise-router')();
+const passport = require('passport');
+require('../passport');
+const { validateBody, schemas } = require('../helpers/routerHelpers');
+const UserController = require('../controllers/users');
+
+const passportSignIn = passport.authenticate('local', { session: false });
+const passportSecretJWT = passport.authenticate('jwt', { session: false });
+const passportGoogle = passport.authenticate('googleToken', { session: false });
+const passportFacebook = passport.authenticate('facebookToken', {
+  session: false,
+});
+
+router
+  .route('/signup')
+  .post(validateBody(schemas.signUpSchema), UserController.signUp);
+
+router
+  .route('/signin')
+  .post(
+    validateBody(schemas.signInSchema),
+    passportSignIn,
+    UserController.signIn
+  );
+
+router.route('/oauth/google').post(passportGoogle, UserController.googleOAuth);
+
+router
+  .route('/oauth/facebook')
+  .post(passportFacebook, UserController.facebookOAuth);
+
+router.route('/secret').get(passportSecretJWT, UserController.secret);
+
+module.exports = router;
--- notes/notes-backend/server.js
+++ notes/notes-backend/server.js
@@ -1,5 +1,29 @@
 const express = require('express');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
+// Connect to DB
+mongoose
+  .connect(process.env.mongoURI, { useNewUrlParser: true })
+  .then(() => console.log('MongoDB connected'));
+
+// Middlewares
 app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+// Routes
+app.use('/auth', require('./routes/users'));
+
+// Start server
+const port = process.env.PORT || 5000;
+app.listen(port);
+console.log(`Server listening on port ${port}`);
